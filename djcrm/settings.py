796f4b0fd53d9d9b078fbd606dd408b7962e9646
Nimrod Chitayi <nimrod.chitayi@gmail.com>
2022-11-01 22:49:39 +0300

    created a leads app and models

--- djcrm/settings.py
+++ djcrm/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    
+    # CRM Apps
+    'leads',
 ]
 
 MIDDLEWARE = [
@@ -121,3 +124,6 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Alert Django that you have a Custom User Model
+AUTH_USER_MODEL = 'leads.User'
